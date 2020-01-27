e1011a29c497e2f28bd2f9c31f4522a1dd05ece8
antoinemadec <ajamadec@gmail.com>
2020-01-27 14:11:38 -0800

    Add doc

---
+++ gen_doc_from_readme.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+FILENAME="$(basename $PWD).txt"
+
+# setup
+git clone https://github.com/xolox/vim-tools.git
+cd vim-tools
+virtualenv html2vimdoc
+html2vimdoc/bin/pip install beautifulsoup coloredlogs markdown
+
+# fix Beatiful Soup error
+set -i 's/logger.addHandler(coloredlogs.ColoredStreamHandler(show_name=True))/coloredlogs.install(level='DEBUG')/' *
+
+# gen vim doc
+html2vimdoc/bin/python ./html2vimdoc.py --file=$FILENAME ../README.md > ../doc/$FILENAME
+
+# hack the commands table
+cd ../doc
+sed -i 's/| Command | List |/| Command | List |~\n/' $FILENAME
+sed -i 's/ | --- | --- | //' $FILENAME
+sed -i "s/'\(:[^:]*\)'/\`\1\`/g" $FILENAME
+echo "INFO: please align vimdoc's Commands table manually"
